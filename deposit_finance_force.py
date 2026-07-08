bcd25951e374c12ce5c42d156fe1384f9d22cba8
kirannayakcontact-spec <kirannayak.contact@gmail.com>
2026-07-08 21:04:47 +0530

    Rollback compact Ledger UI loader

--- deposit_finance_force.py
+++ deposit_finance_force.py
@@ -9,11 +9,6 @@ def register_deposit_finance_force(app):
     except Exception:
         pass
     try:
-        from ledger_compact_ui import register_ledger_compact_ui
-        register_ledger_compact_ui(app)
-    except Exception:
-        pass
-    try:
         from settlement_toggle_sticky import register_settlement_toggle_sticky
         register_settlement_toggle_sticky(app)
     except Exception:
