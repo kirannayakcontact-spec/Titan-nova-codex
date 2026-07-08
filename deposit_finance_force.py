5ce15e664bf1e554bcd079542c10ce6f9bd49eab
kirannayakcontact-spec <kirannayak.contact@gmail.com>
2026-07-08 19:42:20 +0530

    Remove duplicate Ledger market drawer loader

--- deposit_finance_force.py
+++ deposit_finance_force.py
@@ -9,11 +9,6 @@ def register_deposit_finance_force(app):
     except Exception:
         pass
     try:
-        from ledger_market_drawer import register_ledger_market_drawer
-        register_ledger_market_drawer(app)
-    except Exception:
-        pass
-    try:
         from settlement_toggle_sticky import register_settlement_toggle_sticky
         register_settlement_toggle_sticky(app)
     except Exception:
