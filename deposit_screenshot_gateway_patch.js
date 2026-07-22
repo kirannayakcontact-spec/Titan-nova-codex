1cc02d17ccb7864aab9ed8aad24f039c6385837b
kirannayakcontact-spec <kirannayak.contact@gmail.com>
2026-07-22 19:15:50 +0530

    Remove obsolete development and compatibility file

--- deposit_screenshot_gateway_patch.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// Disabled Deposit screenshot Gateway handler.
-// Finance Deposit tab/backend has been removed from Titan Nova runtime.
-// This file intentionally does nothing if accidentally required.
-
-module.exports = { disabled: true, feature: "deposit_screenshot_gateway_patch" };
